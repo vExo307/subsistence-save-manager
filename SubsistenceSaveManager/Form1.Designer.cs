1db92748e2f3983041928b3b347607e6e184d55a
Exxo <exxodevelopment@gmail.com>
2020-08-08 14:40:46 -0500

    Initial Commit

---
+++ SubsistenceSaveManager/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SubsistenceSaveManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currentpathlabel = new System.Windows.Forms.Label();
+            this.backupsLabel = new System.Windows.Forms.Label();
+            this.BackupsList = new System.Windows.Forms.ListBox();
+            this.createbackup = new System.Windows.Forms.Button();
+            this.deletebackup = new System.Windows.Forms.Button();
+            this.deleteallbackups = new System.Windows.Forms.Button();
+            this.restorebackup = new System.Windows.Forms.Button();
+            this.changebackupname = new System.Windows.Forms.Button();
+            this.namechangetext = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // currentpathlabel
+            // 
+            this.currentpathlabel.AutoSize = true;
+            this.currentpathlabel.Location = new System.Drawing.Point(9, 415);
+            this.currentpathlabel.Name = "currentpathlabel";
+            this.currentpathlabel.Size = new System.Drawing.Size(37, 13);
+            this.currentpathlabel.TabIndex = 9;
+            this.currentpathlabel.Text = "{Path}";
+            // 
+            // backupsLabel
+            // 
+            this.backupsLabel.AutoSize = true;
+            this.backupsLabel.Location = new System.Drawing.Point(622, 2);
+            this.backupsLabel.Name = "backupsLabel";
+            this.backupsLabel.Size = new System.Drawing.Size(49, 13);
+            this.backupsLabel.TabIndex = 11;
+            this.backupsLabel.Text = "Backups";
+            // 
+            // BackupsList
+            // 
+            this.BackupsList.FormattingEnabled = true;
+            this.BackupsList.Location = new System.Drawing.Point(590, 18);
+            this.BackupsList.Name = "BackupsList";
+            this.BackupsList.Size = new System.Drawing.Size(120, 407);
+            this.BackupsList.TabIndex = 12;
+            this.BackupsList.SelectedIndexChanged += new System.EventHandler(this.BackupsList_SelectedIndexChanged);
+            // 
+            // createbackup
+            // 
+            this.createbackup.Location = new System.Drawing.Point(12, 18);
+            this.createbackup.Name = "createbackup";
+            this.createbackup.Size = new System.Drawing.Size(106, 23);
+            this.createbackup.TabIndex = 13;
+            this.createbackup.Text = "Create Backup";
+            this.createbackup.UseVisualStyleBackColor = true;
+            this.createbackup.Click += new System.EventHandler(this.createbackup_Click);
+            // 
+            // deletebackup
+            // 
+            this.deletebackup.Enabled = false;
+            this.deletebackup.Location = new System.Drawing.Point(12, 76);
+            this.deletebackup.Name = "deletebackup";
+            this.deletebackup.Size = new System.Drawing.Size(106, 23);
+            this.deletebackup.TabIndex = 14;
+            this.deletebackup.Text = "Delete Backup";
+            this.deletebackup.UseVisualStyleBackColor = true;
+            this.deletebackup.Click += new System.EventHandler(this.deletebackup_Click);
+            // 
+            // deleteallbackups
+            // 
+            this.deleteallbackups.Location = new System.Drawing.Point(12, 47);
+            this.deleteallbackups.Name = "deleteallbackups";
+            this.deleteallbackups.Size = new System.Drawing.Size(106, 23);
+            this.deleteallbackups.TabIndex = 15;
+            this.deleteallbackups.Text = "Delete All Backups";
+            this.deleteallbackups.UseVisualStyleBackColor = true;
+            this.deleteallbackups.Click += new System.EventHandler(this.deleteallbackups_Click);
+            // 
+            // restorebackup
+            // 
+            this.restorebackup.Enabled = false;
+            this.restorebackup.Location = new System.Drawing.Point(12, 105);
+            this.restorebackup.Name = "restorebackup";
+            this.restorebackup.Size = new System.Drawing.Size(106, 23);
+            this.restorebackup.TabIndex = 16;
+            this.restorebackup.Text = "Restore Backup";
+            this.restorebackup.UseVisualStyleBackColor = true;
+            this.restorebackup.Click += new System.EventHandler(this.restorebackup_Click);
+            // 
+            // changebackupname
+            // 
+            this.changebackupname.Enabled = false;
+            this.changebackupname.Location = new System.Drawing.Point(478, 18);
+            this.changebackupname.Name = "changebackupname";
+            this.changebackupname.Size = new System.Drawing.Size(106, 23);
+            this.changebackupname.TabIndex = 17;
+            this.changebackupname.Text = "Change Name";
+            this.changebackupname.UseVisualStyleBackColor = true;
+            this.changebackupname.Click += new System.EventHandler(this.changebackupname_Click);
+            // 
+            // namechangetext
+            // 
+            this.namechangetext.Enabled = false;
+            this.namechangetext.Location = new System.Drawing.Point(349, 21);
+            this.namechangetext.Name = "namechangetext";
+            this.namechangetext.Size = new System.Drawing.Size(123, 20);
+            this.namechangetext.TabIndex = 18;
+            this.namechangetext.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(722, 437);
+            this.Controls.Add(this.namechangetext);
+            this.Controls.Add(this.changebackupname);
+            this.Controls.Add(this.restorebackup);
+            this.Controls.Add(this.deleteallbackups);
+            this.Controls.Add(this.deletebackup);
+            this.Controls.Add(this.createbackup);
+            this.Controls.Add(this.BackupsList);
+            this.Controls.Add(this.backupsLabel);
+            this.Controls.Add(this.currentpathlabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Subsistence Save Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label currentpathlabel;
+        private System.Windows.Forms.Label backupsLabel;
+        private System.Windows.Forms.ListBox BackupsList;
+        private System.Windows.Forms.Button createbackup;
+        private System.Windows.Forms.Button deletebackup;
+        private System.Windows.Forms.Button deleteallbackups;
+        private System.Windows.Forms.Button restorebackup;
+        private System.Windows.Forms.Button changebackupname;
+        private System.Windows.Forms.TextBox namechangetext;
+    }
+}
+
---
+++ SubsistenceSaveManager/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Security.Cryptography;
+
+namespace SubsistenceSaveManager
+{
+    public partial class Form1 : Form
+    {
+        string steamdirectory = null;
+        string currentsave = null;
+        string currentbackup = null;
+
+        string backupdirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\Backups";
+
+        internal static readonly char[] chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        string[] savefilenames =
+        {
+            "saveFilesList.sav",
+            "saveGame1.sav",
+            "saveGame2.sav",
+            "saveGame3.sav",
+            "saveGame4.sav",
+            "saveGame5.sav",
+            "saveGame6.sav",
+            "saveGame7.sav",
+            "saveGame8.sav",
+            "saveGame9.sav",
+            "saveGame10.sav",
+            "saveGame11.sav",
+            "saveGame12.sav",
+            "saveGame13.sav",
+            "saveGame14.sav",
+            "saveGame15.sav",
+            "saveGame16.sav",
+            "saveGame17.sav",
+            "saveGame18.sav",
+            "saveGame19.sav",
+            "saveGame20.sav",
+            "saveGame21.sav",
+            "saveGame22.sav",
+            "saveGame23.sav",
+            "saveGame24.sav",
+            "saveGame25.sav",
+            "saveGame26.sav",
+            "saveGame27.sav",
+            "saveGame28.sav",
+            "saveGame29.sav",
+            "saveGame30.sav",
+            "msp1.sav"
+        };
+
+        public static string GetUniqueKey(int size)
+        {
+            byte[] data = new byte[4 * size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                var rnd = BitConverter.ToUInt32(data, i * 4);
+                var idx = rnd % chars.Length;
+
+                result.Append(chars[idx]);
+            }
+
+            return result.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void makeConfig()
+        {
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager");
+            }
+
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\Backups"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\Backups");
+            }
+
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml"))
+            {
+                using (FileStream fs = File.Create(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml"))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("<?xml version=\"1.0\"?>\n<Config>\n<SteamPath></SteamPath>\n</Config>");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+        }
+
+        public void makeBackupsList()
+        {
+            BackupsList.Items.Clear();
+            DirectoryInfo d = new DirectoryInfo(backupdirectory);
+            DirectoryInfo[] Files = d.GetDirectories();
+            foreach (DirectoryInfo file in Files)
+            {
+                BackupsList.Items.Add(file.Name);
+            }
+        }
+
+        string defaultSteamFolder = Environment.ExpandEnvironmentVariables("%ProgramW6432%") + "\\Steam\\steamapps\\common\\Subsistence";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager") && Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\Backups") && File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml"))
+            {
+                XmlDocument xmlDoc = new XmlDocument(); // Create an XML document object
+                xmlDoc.Load(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml"); // Load the XML document from the specified file
+
+                XmlNodeList sp = xmlDoc.GetElementsByTagName("SteamPath");
+
+                steamdirectory = sp[0].InnerText;
+
+            }
+            else
+            {
+                makeConfig();
+
+                if (Directory.Exists(defaultSteamFolder))
+                {
+                    steamdirectory = defaultSteamFolder;
+                }
+                else
+                {
+                    using (var fbd = new FolderBrowserDialog())
+                    {
+                        fbd.Description = "Select your subsistence folder";
+                        DialogResult result = fbd.ShowDialog();
+
+                        if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                        {
+                            steamdirectory = fbd.SelectedPath;
+                            XmlDocument xmlDoc = new XmlDocument(); // Create an XML document object
+                            xmlDoc.Load(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml"); // Load the XML document from the specified file
+
+                            XmlNodeList sp = xmlDoc.GetElementsByTagName("SteamPath");
+
+                            sp[0].InnerText = fbd.SelectedPath;
+
+                            xmlDoc.Save(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SubSaveManager\\config.xml");
+
+                        }
+                    }
+                }
+            }
+            currentpathlabel.Text = "Game Path: " + steamdirectory;
+            makeBackupsList();
+        }
+
+        private void BackupsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            deletebackup.Enabled = true;
+            restorebackup.Enabled = true;
+            changebackupname.Enabled = true;
+            namechangetext.Enabled = true;
+            currentbackup = BackupsList.GetItemText(BackupsList.SelectedItem);
+            namechangetext.Text = "";
+        }
+
+        private void createbackup_Click(object sender, EventArgs e)
+        {
+            makeBackupsList();
+            string backupD = GetUniqueKey(5);
+            Directory.CreateDirectory(backupdirectory + "\\" + backupD);
+            foreach (string f in savefilenames)
+            {
+                if (File.Exists(steamdirectory + "\\Binaries\\Win32\\" + f)) {
+                    File.Copy(steamdirectory + "\\Binaries\\Win32\\" + f, backupdirectory + "\\" + backupD + "\\" + f);
+                }
+                if (File.Exists(steamdirectory + "\\Binaries\\Win32\\MapData\\" + f))
+                {
+                    File.Copy(steamdirectory + "\\Binaries\\Win32\\MapData\\" + f, backupdirectory + "\\" + backupD + "\\" + f);
+                }
+            }
+            makeBackupsList();
+        }
+
+        private void deletebackup_Click(object sender, EventArgs e)
+        {
+            makeBackupsList();
+            Directory.Delete(backupdirectory + "\\" + currentbackup, true);
+            deletebackup.Enabled = false;
+            restorebackup.Enabled = false;
+            changebackupname.Enabled = false;
+            namechangetext.Enabled = false;
+            namechangetext.Text = "";
+            makeBackupsList();
+        }
+
+        private void deleteallbackups_Click(object sender, EventArgs e)
+        {
+            makeBackupsList();
+            DirectoryInfo d = new DirectoryInfo(backupdirectory);
+            DirectoryInfo[] Files = d.GetDirectories();
+            foreach (DirectoryInfo file in Files)
+            {
+                Directory.Delete(backupdirectory + "\\" + file.Name);
+            }
+            deletebackup.Enabled = false;
+            restorebackup.Enabled = false;
+            changebackupname.Enabled = false;
+            namechangetext.Enabled = false;
+            namechangetext.Text = "";
+            makeBackupsList();
+        }
+
+        private void restorebackup_Click(object sender, EventArgs e)
+        {
+            foreach (string f in savefilenames)
+            {
+                if (File.Exists(backupdirectory + "\\" + currentbackup + "\\" + f))
+                {
+                    if (f.ToString() != "msp1.sav")
+                    {
+                        File.Copy(backupdirectory + "\\" + currentbackup + "\\" + f, steamdirectory + "\\Binaries\\Win32\\" + f, true);
+                    }
+                    else
+                    {
+                        File.Copy(backupdirectory + "\\" + currentbackup + "\\" + f, steamdirectory + "\\Binaries\\Win32\\MapData\\" + f, true);
+                    }
+                }
+            }
+        }
+
+        private void changebackupname_Click(object sender, EventArgs e)
+        {
+            makeBackupsList();
+            try
+            {
+              Directory.Move(backupdirectory + "\\" + currentbackup, backupdirectory + "\\" + namechangetext.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Name!");
+                namechangetext.Text = "";
+                return;
+            }
+            namechangetext.Text = "";
+            makeBackupsList();
+        }
+    }
+       
+}
